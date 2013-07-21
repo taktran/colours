ff45b909c55b539a56e3e65e371e2a5beace306b
Tak Tran <contact@tutaktran.com>
2013-07-21 07:16:03 +0100

    Register player 1 and 2 input.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 (function (){
   'use strict';
 
-  // There's some code in here
+  $(".player input[type=range]").change(function(data) {
+    var el = $(data.target),
+      val = el.val(),
+      playerNum = el.parent().data("player-num");
+
+    console.log(playerNum, val);
+  });
 
 })();
