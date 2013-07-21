7c3c42bab1da80264ee2f6b4a8eaafe4babc4bb4
Tak Tran <contact@tutaktran.com>
2013-07-21 21:38:16 +0100

    Clean up arduino potentiometer file.

--- arduino/potentiometer/potentiometer.ino
+++ arduino/potentiometer/potentiometer.ino
@@ -1,50 +1,30 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13.
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead().
+  Potentiometer input
 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
-
- * Note: because most Arduinos have a built-in LED attached
- to pin 13 on the board, the LED is optional.
-
-
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
-
- This example code is in the public domain.
-
- http://arduino.cc/en/Tutorial/AnalogInput
-
+  Based on code from: http://arduino.cc/en/Tutorial/AnalogInput
  */
 
-int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorPin = A0;
 int sensorPin2 = A1;
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+
+int prevSensorValue = 0;
+int sensorValue = 0;
+int prevSensor2Value = 0;
 int sensorValue2 = 0;
+
+int ledPin = 13;
 
 #include <stdarg.h>
 void p(char *fmt, ... ){
-        char tmp[128]; // resulting string limited to 128 chars
-        va_list args;
-        va_start (args, fmt );
-        vsnprintf(tmp, 128, fmt, args);
-        va_end (args);
-        Serial.print(tmp);
+  char tmp[128]; // resulting string limited to 128 chars
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(tmp, 128, fmt, args);
+  va_end (args);
+  Serial.print(tmp);
 }
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
@@ -54,14 +34,24 @@ void loop() {
   sensorValue = analogRead(sensorPin);
   sensorValue2 = analogRead(sensorPin2);
 
-  Serial.println(sensorValue);
-//  p("%i %i\n", sensorValue, sensorValue2);
+  bool sensorHasChanged = (prevSensorValue != sensorValue);
+  bool sensor2HasChanged = (prevSensor2Value != sensorValue2);
+  if (sensorHasChanged || sensor2HasChanged) {
+    p("%i %i\n", sensorValue, sensorValue2);
+  }
 
-  // turn the ledPin on
+  if (sensorHasChanged) {
+    prevSensorValue = sensorValue;
+  }
+
+  if (sensor2HasChanged) {
+    prevSensor2Value = sensorValue2;
+  }
+
+  // Flash light for effect
   digitalWrite(ledPin, HIGH);
-  delay(100);
-  // turn the ledPin off:
+  delay(50);
   digitalWrite(ledPin, LOW);
-  // stop the program for for <sensorValue> milliseconds:
-  delay(100);
+  delay(50);
+
 }
