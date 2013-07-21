53abf207f825b5103f88c85431224659ca7f7c95
Tak Tran <contact@tutaktran.com>
2013-07-21 08:46:30 +0100

    Add goal check.

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,20 @@
 (function (){
   'use strict';
 
-  var NUM_PLAYERS = 2;
+  var NUM_PLAYERS = 2,
+    goal;
 
   function init() {
     var player1El = $(".player-1 input"),
       player2El = $(".player-2 input"),
       randomInput1 = random(player1El.attr("min"), player1El.attr("max")),
-      randomInput2 = random(player2El.attr("min"), player2El.attr("max")),
-      goalEl = $(".combined .goal");
+      randomInput2 = random(player2El.attr("min"), player2El.attr("max"));
 
     $(".player-1 input").val(randomInput1);
     $(".player-2 input").val(randomInput2);
 
     // Random goal
-    goalEl.css("background-color", randomRGB(player1El.attr("min"), player1El.attr("max")));
+    randomGoal(player1El.attr("min"), player1El.attr("max"));
 
     updateColours();
 
@@ -27,11 +27,20 @@
     return Math.floor((Math.random() * parseInt(max, 10)) + parseInt(min, 10));
   }
 
-  function randomRGB(min, max) {
-    var randomVal1 = random(min, max),
-      randomVal2 = random(min, max);
+  function randomGoal(min, max) {
+    var goalEl = $(".combined .goal"),
+      randomVal1 = random(min, max),
+      randomVal2 = random(min, max),
+      goalRGB;
 
-    return "rgb(" + randomVal1 + ", 0, " + randomVal2 + ")";
+    goal = { r: randomVal1, g: 0, b: randomVal2 };
+    goalRGB = rgbCSS(goal);
+
+    goalEl.css("background-color", goalRGB);
+  }
+
+  function rgbCSS(rgbHash) {
+    return "rgb(" + rgbHash.r + ", " + rgbHash.g +", " + rgbHash.b + ")";
   }
 
   function combinedColour() {
@@ -44,6 +53,16 @@
   function updateCombinedColour() {
     var combinedEl = $(".combined .current");
     combinedEl.css("background-color", combinedColour());
+  }
+
+  function goalCheck() {
+    var player1Colour = $(".player-1 input").val(),
+      player2Colour = $(".player-2 input").val();
+
+    return player1Colour === goal.r &&
+      0 === goal.g &&
+      player2Colour === goal.b;
+
   }
 
   function updateColours() {
@@ -62,7 +81,7 @@
       playerEl.css("background-color", playerColour);
       updateCombinedColour();
 
-      console.log("update", n, playerInput, playerColour);
+      console.log("update", n, playerInput, playerColour, goal, goalCheck());
     }
   }
 
