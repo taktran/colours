93a2440fe8fc464cbf8dc6e918d88493d9e5fa85
Tak Tran <contact@tutaktran.com>
2013-07-23 01:38:05 +0100

    Pull out potentiometer as a library that emits on the `read` event.

--- bin/potentiometer.js
+++ bin/potentiometer.js
@@ -10,52 +10,18 @@
 
 var five = require("johnny-five"),
   board = new five.Board({ port: "/dev/tty.usbserial-A800ep51" }),
+  Potentiometer = require("../lib/Potentiometer").Potentiometer,
   pot, pot2,
   printPots = function () {
     console.log("{ sensor1: " + pot + ", sensor2: " + pot2 + " }");
   };
 
-var Potentiometer = function(sensorPin) {
-  var self = this;
-
-  self.val = 0;
-  self.prev = 0;
-
-  self.sensor = new five.Sensor({
-    pin: sensorPin,
-    freq: 250
-  });
-
-  self.sensor.on("read", function( err, value ) {
-    self.val = value;
-
-    if ( self.hasChanged() ) {
-      printPots();
-      self.prev = self.val;
-    }
-  });
-};
-
-// Inject the `sensor` hardware into
-// the Repl instance's context.
-// Allows direct command line access
-Potentiometer.prototype.injectIntoRepl = function(board) {
-  board.repl.inject({
-    pot: this.sensor
-  });
-};
-
-Potentiometer.prototype.hasChanged = function() {
-  return this.val !== this.prev;
-};
-
-Potentiometer.prototype.toString = function() {
-  return this.val;
-};
-
 board.on("ready", function() {
   pot = new Potentiometer("A0");
   pot2 = new Potentiometer("A1");
+
+  pot.on("read", printPots);
+  pot2.on("read", printPots);
 
   pot.injectIntoRepl(board);
   pot2.injectIntoRepl(board);
